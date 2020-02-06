c8fb2368eddd038cbc19af1aff502382c35a6b0b
Yagdra <43428247+Yagdra-nsk@users.noreply.github.com>
2020-02-06 22:52:39 +0700

    Add files via upload

--- Task_1/src/FactoryTest.java
+++ Task_1/src/FactoryTest.java
@@ -12,13 +12,58 @@ public class FactoryTest extends Assert{
     }
 
     @Test
-    public void test(){
+    public void test_1(){
         factory.makeCommand("DEFINE a 4".split(" ")).do_command();
         factory.makeCommand("PUSH a".split(" ")).do_command();
         factory.makeCommand("SQRT".split(" ")).do_command();
-        factory.makeCommand("PRINT".split(" ")).do_command();
 
         //В стеке должно остаться значение 2
         assertEquals(2, factory.getStack().peek(), 0.01);//так как у нас имеются вещ. числа, то сравнение выполняется с погрешностью
+    }
+
+    @Test
+    public void test_2(){
+        factory.makeCommand("DEFINE a 5".split(" ")).do_command();
+        factory.makeCommand("DEFINE b 2".split(" ")).do_command();
+        factory.makeCommand("PUSH b".split(" ")).do_command();
+        factory.makeCommand("PUSH a".split(" ")).do_command();
+        factory.makeCommand("DIVISION".split(" ")).do_command();
+
+        assertEquals(2.5, factory.getStack().peek(), 0.01);
+    }
+
+    @Test
+    public void test_3(){
+        factory.makeCommand("PUSH 500".split(" ")).do_command();
+        factory.makeCommand("PUSH 25".split(" ")).do_command();
+        factory.makeCommand("SQRT".split(" ")).do_command();
+        factory.makeCommand("MULTI".split(" ")).do_command();
+
+        assertEquals(2500, factory.getStack().peek(), 0.01);
+    }
+
+    @Test
+    public void test_4(){
+        factory.makeCommand("DEFINE a 56".split(" ")).do_command();
+        factory.makeCommand("DEFINE c 89".split( " ")).do_command();
+        factory.makeCommand("DEFINE e 2.71".split(" ")).do_command();
+
+        factory.makeCommand("PUSH a".split(" ")).do_command();
+        factory.makeCommand("PUSH c".split(" ")).do_command();
+        factory.makeCommand("PUSH e".split(" ")).do_command();
+
+        assertEquals(2.71, factory.getStack().pop(), 0.01);
+        assertEquals(89, factory.getStack().pop(), 0.01);
+        assertEquals(56, factory.getStack().pop(), 0.01);
+    }
+
+    @Test
+    public void test_5(){
+        factory.makeCommand("POP".split(" ")).do_command();
+        factory.makeCommand("POP".split(" ")).do_command();
+        factory.makeCommand("POP".split(" ")).do_command();
+
+        boolean actual = factory.getStack().isEmpty();
+        assertTrue(actual);
     }
 }
