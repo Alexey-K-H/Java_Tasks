91515529d1e181d17eb39e0e251281d3d56296cd
Yagdra <43428247+Yagdra-nsk@users.noreply.github.com>
2020-02-11 20:31:36 +0700

    Add files via upload

--- Task_1/src/Main.java
+++ Task_1/src/Main.java
@@ -11,27 +11,47 @@ public class Main {
         String arguments;
         Command command;
 
-        try{
-            log.info("Open input file...");
-            File file = new File("input.txt");
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+        if(args.length == 1) {
+            try {
+                log.info("Open input file...");
+                File file = new File(args[0]);
+                FileReader fileReader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            while (true){
-                try{
-                    if((arguments = bufferedReader.readLine()) == null){
+                while (true) {
+                    try {
+                        if ((arguments = bufferedReader.readLine()) == null) {
+                            break;
+                        } else {
+                            command = factory.makeCommand(arguments.split(" "));
+                            if (command != null)
+                                command.do_command();
+                        }
+                    } catch (IOException ex) {
+                        log.severe("Error during reading file!");
+                    }
+                }
+            } catch (Exception ex) {
+                log.severe("File cannot be open!");
+            }
+        }
+        else {
+            BufferedReader Reader = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                while (true){
+                    arguments = Reader.readLine();
+                    if(arguments.equals("")){
                         break;
-                    }else {
+                    }
+                    else{
                         command = factory.makeCommand(arguments.split(" "));
                         if(command != null)
                             command.do_command();
                     }
-                }catch (IOException ex){
-                    log.severe("Error during reading file!");
                 }
+            }catch (IOException ex){
+                log.severe("Error during reading console!");
             }
-        }catch (Exception ex){
-            log.severe("File cannot be open!");
         }
     }
 }
